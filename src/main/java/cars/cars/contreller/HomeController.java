cd921e50155f9ed4f6dc3dbcef6b3c713e35ed3a
n_zhuchkevich <nikitazhuchkevich@gmail.com>
2020-07-01 21:02:58 +0200

    for merge

--- src/main/java/cars/cars/contreller/HomeController.java
+++ src/main/java/cars/cars/contreller/HomeController.java
@@ -1,8 +1,10 @@
 package cars.cars.contreller;
 
+import cars.cars.model.Exaple;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,7 +12,15 @@ public class HomeController {
     @GetMapping("/")
     public String home(final Model model) {
         model.addAttribute("message", "EEEEEE SPRING!!!");
+        final Exaple exaple = new Exaple("AAAAAAAAAa", 15);
+        model.addAttribute("objectForExample", exaple);
+        model.addAttribute("exaple", new Exaple());
         return "home";
+    }
+
+    @PostMapping("/processForm")
+    public void processForm(final Exaple exaple, final Model model) {
+        model.addAttribute("Name", exaple.getName());
     }
 
 }
---
+++ src/main/java/cars/cars/model/Exaple.java
@@ -0,0 +1,18 @@
+package cars.cars.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Exaple {
+
+
+    private String name;
+    private Integer order;
+}
